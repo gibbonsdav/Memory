5fea594fd8f607a3694e8d206c20bf2953522694
David Gibbons <gibbonsdav@gmail.com>
2020-02-13 16:55:06 -0800

    randomizing works

---
+++ array.js
@@ -0,0 +1,43 @@
+// var items = [
+//   { url: "assets/chewy.jpg" },
+//   { url: "assets/chewy.jpg" },
+
+//   { url: "assets/yoda.jpg" },
+//   { url: "assets/yoda.jpg" },
+
+//   { url: "assets/luke.jpg" },
+
+//   { url: "assets/luke.jpg" },
+
+//   { url: "assets/darthvader.jpg" },
+
+//   { url: "assets/darthvader.jpg" },
+
+//   { url: "assets/palpatine.jpg" },
+//   { url: "assets/palpatine.jpg" },
+
+//   { url: "assets/hansolo.jpg" },
+//   { url: "assets/hansolo.jpg" },
+
+//   { url: "assets/leia.jpg" },
+//   { url: "assets/leia.jpg" },
+
+//   { url: "assets/r2.jpg" },
+//   { url: "assets/r2.jpg" },
+
+//   { url: "assets/chewy.jpg" },
+//   { url: "assets/chewy.jpg" },
+
+//   { url: "assets/c3po.jpg" },
+//   { url: "assets/c3po.jpg" }
+// ]
+
+// '<img src="assets/chewy.jpg" />',
+// '<img src="assets/yoda.jpg" />',
+// <img src="assets/luke.jpg" />,
+// <img src="assets/darthvader.jpg" />,
+// <img src="assets/palapatine.jpg" />,
+// <img src="assets/hansolo.jpg" />,
+// <img src="assets/leia.jpg" />,
+// <img src="assets/r2.jpg" />,
+// <img src="assets/c3po.jpg" />
--- main.js
+++ main.js
@@ -1,20 +1,98 @@
 // $(document).ready(function() {
-$("#card1").flip()
-$("#card2").flip()
-$("#card3").flip()
-$("#card4").flip()
-$("#card5").flip()
-$("#card6").flip()
-$("#card7").flip()
-$("#card8").flip()
-$("#card9").flip()
+//   const html = items
+//   .map(function(item, i) {
+//     return `
+//     <div id="card-8" class="card">
+//       <div class="front">
+//         <img src="assets/starwars.jpg" />
+//       </div>
+//       <div class="back">
+//         <img src="assets/r2.jpg" />
+//       </div>
+//     </div>
 
-$("#card-1").flip()
-$("#card-2").flip()
-$("#card-3").flip()
-$("#card-4").flip()
-$("#card-5").flip()
-$("#card-6").flip()
-$("#card-7").flip()
-$("#card-8").flip()
-$("#card-9").flip()
+// `
+//   }
+
+//     var img = ""
+// //Function for shuffling images.
+// function randImg() {
+//   img = results[Math.floor(Math.random() * results.length)]
+// }
+// randImg()
+// // Function End.
+// var items = results
+//   .map(function(item, i) {
+//     return `
+// <div id="card${i}">
+//   <div class="front">
+//   </div>
+//   <div class="back">
+//       <img src="${item.url}">
+//   </div>
+// </div>
+// `
+//   })
+//   .join("")
+// document.querySelector("#cardgame").innerHTML = items
+
+//   document.querySelector(".back").innerHTML = html
+
+//   $("#card1").flip()
+//   $("#card2").flip()
+//   $("#card3").flip()
+//   $("#card4").flip()
+//   $("#card5").flip()
+//   $("#card6").flip()
+//   $("#card7").flip()
+//   $("#card8").flip()
+//   $("#card9").flip()
+
+//   $("#card-1").flip()
+//   $("#card-2").flip()
+//   $("#card-3").flip()
+//   $("#card-4").flip()
+//   $("#card-5").flip()
+//   $("#card-6").flip()
+//   $("#card-7").flip()
+//   $("#card-8").flip()
+//   $("#card-9").flip()
+// })
+
+var cards = $(".card").toArray()
+
+function shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex
+  //While there remain elements to shuffle...
+  while (currentIndex !== 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex -= 1
+    //     // And swap it with the current element.
+    temporaryValue = array[currentIndex]
+    array[currentIndex] = array[randomIndex]
+    array[randomIndex] = temporaryValue
+  }
+  return array
+}
+shuffle(cards)
+console.log(cards)
+$("#memory").html(cards)
+$("#memory")
+  .find(".card")
+  .flip() /
+  $("#memory").on("click", ".card", function(e) {
+    e.preventDefault()
+    if (card1) {
+      card2 = $(this)
+      if (card1.find(".front").html() === card2.find(".front").html()) {
+      } else {
+        card1.flip()
+        card2.flip()
+      }
+    } else {
+      card1 = $(this)
+    }
+  })
